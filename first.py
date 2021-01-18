2c3826edec05cfc64fc797a6a8619403b8b6a884
geba- <geba_m@mail.ru>
2021-01-18 12:24:13 +0700

    Finish

--- first.py
+++ first.py
@@ -16,7 +16,8 @@ def load_level(filename):
 
 
 def settings():
-    screen.blit(load_image('settings_screen.png'), (400, 100))
+    im = load_image('settings_screen.png')
+    backgournd = load_image('background.png')
     exit_button1 = Button('icon_button', 420, 120, 120, 120, pygame.transform.scale(load_image('exit.png'), (100, 100)))
     volume_none = Button('volume_none', 500, 300, 58, 58, pygame.transform.scale(load_image('volume_none.png'),
                                                                                  (100, 100)))
@@ -24,9 +25,10 @@ def settings():
                                                                              (120, 100)))
     volume50 = Button('volume50', 800, 300, 58, 58, pygame.transform.scale(load_image('volume50.png'),
                                                                            (100, 100)))
+    screen.blit(backgournd, (0, 0))
+    screen.blit(im, (400, 100))
     while True:
         global stop
-        screen.blit(load_image('settings_screen.png'), (400, 100))
         exit_button1.draw()
         volume_none.draw()
         volume100.draw()
@@ -36,6 +38,7 @@ def settings():
                 return
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if exit_button1.isMotion():
+                    stop = True
                     start_screen()
                     return
                 if volume_none.isMotion():
@@ -48,8 +51,8 @@ def settings():
                 if event.key == pygame.K_ESCAPE:
                     if cube:
                         stop = True
-                        return
-
+                    start_screen()
+                    return
         pygame.display.flip()
         clock.tick(180)
 
@@ -61,6 +64,8 @@ def start_screen():
     icon_button = Button('icon_button', 481, 400, 213, 213, load_image('icon_button.png'))
     main_button = Button('main_button', 785, 332, 320, 320, load_image('main_button.png'))
     settings_button = Button('settings_button', 1200, 400, 200, 200, load_image('settings.png'))
+    pygame.mixer.music.load('data/menu_sound.mp3')
+    pygame.mixer.music.play()
 
     while True:
         settings_button.draw()
@@ -72,7 +77,8 @@ def start_screen():
                 return
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if icon_button.isMotion():
-                    icon_button.isPressed()
+                    icon_screen()
+                    return
                 if main_button.isMotion():
                     main_button.isPressed()
                     level_screen()
@@ -90,23 +96,39 @@ def start_screen():
                     return
         icon_button.changeIm()
         main_button.changeIm()
+        settings_button.changeIm()
         pygame.display.flip()
         clock.tick(180)
 
 
 def end_screen():
-    screen.blit(load_image('end_screen.png'), (400, 100))
+    global stop, first_do, second_do
+    if name == 'level1.txt':
+        first_do = True
+    elif name == 'level2.txt':
+        second_do = True
+    im = load_image('end_screen.png')
+    y = -1080
     exit_button2 = Button('icon_button', 420, 120, 120, 120, pygame.transform.scale(load_image('exit.png'), (100, 100)))
     while True:
-        exit_button2.draw()
+        draw_background(name)
+        screen.blit(im, (400, y))
+        if y != 100:
+            y += 20
+        else:
+            exit_button2.draw()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             if exit_button2.isMotion():
+                exit_button2.isPressed()
+                start_screen()
                 return
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    stop = True
+                    start_screen()
+                    return
         pygame.display.flip()
         clock.tick(180)
 
@@ -116,12 +138,20 @@ def level_screen():
     im = load_image('level_screen.png')
     exit_button = Button('icon_button', 10, 10, 120, 120, pygame.transform.scale(load_image('exit.png'), (100, 100)))
     first_level_button = Button('first_level_button', 140, 150, 1550, 290,
-                                pygame.transform.scale(load_image('first_level.png'),
-                                                       (100, 100)))
+                                load_image('first_level.png'))
+
+    second_level_button = Button('second_level_button', 140, 550, 1550, 290, load_image('second_level.png'))
+    check = load_image('check.png')
+
     while True:
         screen.blit(im, (0, 0))
         exit_button.draw()
         first_level_button.draw()
+        second_level_button.draw()
+        if first_do:
+            screen.blit(check, (1600, 150))
+        if second_do:
+            screen.blit(check, (1600, 550))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -132,7 +162,11 @@ def level_screen():
                 if first_level_button.isMotion():
                     name = 'level1.txt'
                     generate_level(name)
-                    pygame.mixer.music.stop()
+                    play_sound.play()
+                    return
+                if second_level_button.isMotion():
+                    name = 'level2.txt'
+                    generate_level(name)
                     play_sound.play()
                     return
             if event.type == pygame.KEYDOWN:
@@ -141,32 +175,32 @@ def level_screen():
                     start_screen()
                     return
         first_level_button.changeIm()
+        second_level_button.changeIm()
         pygame.display.flip()
         clock.tick(180)
 
 
 def icon_screen():
     screen.blit(pygame.transform.scale(cube.image, (310, 310)), (820, 160))
-    main_icon = Icon('player_icon1', 377, 510, pygame.transform.scale(load_image('player_icon1.png'), (124, 124)))
     player_icon1 = Icon('player_icon1', 377, 510, pygame.transform.scale(load_image('player_icon1.png'), (124, 124)))
     player_icon2 = Icon('player_icon2', 530, 510, pygame.transform.scale(load_image('player_icon2.png'), (124, 124)))
     player_icon3 = Icon('player_icon3', 685, 510, pygame.transform.scale(load_image('player_icon3.png'), (124, 124)))
     while True:
         screen.blit(load_image('icon_center.png'), (0, 0))
-        screen.blit(pygame.transform.scale(main_icon.image, (155, 155)), (820, 160))
+        screen.blit(pygame.transform.scale(cube.image, (155, 155)), (820, 160))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start_screen()
+                return
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if player_icon1.isMotion():
-                    player_icon1.isPressed(main_icon)
+                    player_icon1.isPressed()
                 if player_icon2.isMotion():
-                    player_icon2.isPressed(main_icon)
+                    player_icon2.isPressed()
                 if player_icon3.isMotion():
-                    player_icon3.isPressed(main_icon)
+                    player_icon3.isPressed()
                 x, y = event.pos
                 if 0 <= x <= 150 and 0 <= y <= 150:
-                    quit_sound.play()
                     start_screen()
                     return
             if event.type == pygame.KEYDOWN:
@@ -183,7 +217,7 @@ def icon_screen():
 
 def generate_level(name):
     global len_level
-    level = load_level(name)[:-1]
+    level = load_level(name)
     len_level = len(level[1]) * 100
     for i in range(len(level)):
         for j in range(len(level[i])):
@@ -223,8 +257,8 @@ class Cube(pygame.sprite.Sprite):
             make_jump = False
 
     def update(self):
-        global min_y
-        cube.image = main_icon.image
+        global min_y, im
+        im = self.image
         self.rect = self.rect.move(0, self.vy)
         if self.rect.y > min_y:
             self.rect.y = min_y
@@ -288,13 +322,15 @@ class Spike(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
-        global running, stop
+        global running, stop, im
         self.rect = self.rect.move(-5, 0)
         if pygame.sprite.collide_mask(self, cube):
+            death_sound.play()
+            im = cube.image
             stop = True
-        if isWave and wave:
+        if isWave and wave != 0:
             if pygame.sprite.spritecollideany(self, wave_sprite):
-                sound1.play()
+                death_sound.play()
                 stop = True
 
 
@@ -306,18 +342,19 @@ class Map(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = x, y
 
     def update(self):
-        global stop
+        global stop, im
         self.rect = self.rect.move(-5, 0)
         if pygame.sprite.collide_mask(self, cube):
             if cube.rect.y <= self.rect.y - 10:
                 cube.rect.y = self.rect.y - 100
                 cube.onGround = True
             else:
-                sound1.play()
+                death_sound.play()
+                im = cube.image
                 stop = True
-        if isWave and wave:
+        if isWave and wave != 0:
             if pygame.sprite.spritecollideany(self, wave_sprite):
-                sound1.play()
+                death_sound.play()
                 stop = True
 
 
@@ -336,6 +373,10 @@ class Button():
                 self.image = load_image('main_button_true.png')
             elif self.name == 'first_level_button':
                 self.image = load_image('first_level_true.png')
+            elif self.name == 'second_level_button':
+                self.image = load_image('second_level_true.png')
+            elif self.name == 'settings_button':
+                self.image = load_image('settings_true.png')
         else:
             if self.name == 'icon_button':
                 self.image = load_image('icon_button.png')
@@ -343,21 +384,26 @@ class Button():
                 self.image = load_image('main_button.png')
             elif self.name == 'first_level_button':
                 self.image = load_image('first_level.png')
+            elif self.name == 'second_level_button':
+                self.image = load_image('second_level.png')
+            elif self.name == 'settings_button':
+                self.image = load_image('settings.png')
 
     def isMotion(self):
         x, y = pygame.mouse.get_pos()
         return self.x <= x <= self.x + self.sizex and self.y <= y <= self.y + self.sizey
 
     def isPressed(self):
+        global stop
         if self.isMotion():
-            if self.name == 'icon_button':
-                icon_screen()
             if self.name == 'volume_none':
                 pygame.mixer.music.set_volume(0.0)
             if self.name == 'volume100':
                 pygame.mixer.music.set_volume(1)
             if self.name == 'volume50':
                 pygame.mixer.music.set_volume(0.5)
+            if self.name == 'exit_button2':
+                stop = True
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
@@ -374,9 +420,9 @@ class Icon():
         x, y = pygame.mouse.get_pos()
         return self.x <= x <= self.x + 124 and self.y <= y <= self.y + 124
 
-    def isPressed(self, obj):
+    def isPressed(self):
         if self.isMotion():
-            main_icon.image = pygame.transform.scale(self.image, (100, 100))
+            cube.image = pygame.transform.scale(self.image, (100, 100))
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
@@ -392,68 +438,65 @@ class Portal(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
-        global isWave, wave
+        global isWave, wave, make_jump
         self.rect = self.rect.move(-5, 0)
+        if self.rect.x + 100 == cube.rect.x and self.rect.y < cube.rect.y < self.rect.y + 200 and self.id == 1:
+            wave = Wave(wave_sprite)
+            isWave = True
+        if wave != 0:
+            if self.rect.x == wave.rect.x + 100 and self.rect.y - 100 < wave.rect.y < self.rect.y + 200 and self.id == 0:
+                cube.rect.y = wave.rect.y - 50
+                cube1.rect.y = wave.rect.y - 150
+                cube.vy = 5
+                make_jump = False
+
+                wave = 0
+                isWave = False
 
 
-def draw_background():
-    pygame.draw.rect(screen, 'pink', (0, 0, 1920, 1080))
+def draw_background(name):
+    if name == 'level1.txt':
+        pygame.draw.rect(screen, 'pink', (0, 0, 1920, 1080))
+        pygame.draw.rect(screen, '#FF69B4', (0, 0, 1920, 80))
+        pygame.draw.rect(screen, 'black', (0, 0, 1920, 80), 5)
+    if name == 'level2.txt':
+        pygame.draw.rect(screen, '#3CB371', (0, 0, 1920, 1080))
+        pygame.draw.rect(screen, '#008000', (0, 0, 1920, 80))
+        pygame.draw.rect(screen, 'black', (0, 0, 1920, 80), 5)
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Жёлтый круг')
+    pygame.display.set_caption('Yandex dash')
     size = width, height = 1920, 1080
     screen = pygame.display.set_mode(size)
     pygame.display.set_icon(load_image('icon.bmp'))
-    cube_sprite = pygame.sprite.Group()
-    map_sprite = pygame.sprite.Group()
-    spike_sprite = pygame.sprite.Group()
-    wave_sprite = pygame.sprite.Group()
-    portal_sprite = pygame.sprite.Group()
-    check_cube = pygame.sprite.Group()
-    cube = Cube(cube_sprite)
-    stop = False
-    main_icon = Icon('player_icon1', 377, 510, pygame.transform.scale(load_image('player_icon1.png'), (100, 100)))
-    countJump = 40
-    cube1 = Check_cube(check_cube)
-    wave = 0
-    isWave = False
-    make_jump = False
-    min_y = 980
-    name, len_level = '', ''
-    x = 0
-    clock = pygame.time.Clock()
+    cube_sprite, map_sprite, spike_sprite = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
+    wave_sprite, portal_sprite, check_cube = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
+    cube, stop = Cube(cube_sprite), False
+    im, countJump, wave, isWave, make_jump, min_y, name, len_level, x = 0, 40, 0, False, False, 980, '', '', 0
+    cube1, clock = Check_cube(check_cube), pygame.time.Clock()
     pygame.mixer.music.load('data/menu_sound.mp3')
+    first_do, second_do = False, False
     running = True
     pygame.mixer.music.play()
-    quit_sound = pygame.mixer.Sound('Data/quit.ogg')
-    sound1 = pygame.mixer.Sound('data/Death.mp3')
-    pygame.mixer.music.set_volume(0.1)
+    quit_sound, death_sound = pygame.mixer.Sound('Data/quit.ogg'), pygame.mixer.Sound('data/Death.mp3')
+    pygame.mixer.music.set_volume(1)
     play_sound = pygame.mixer.Sound('Data/play.ogg')
     start_screen()
-    pygame.mixer.music.stop()
-    inPortal = False
-    pygame.mixer.music.load('Data/level1.mp3')
-    pygame.mixer.music.play()
-    pygame.mixer.music.set_volume(0.1)
+    if running:
+        pygame.mixer.music.load(f'Data/{name[:6]}.mp3')
+        pygame.mixer.music.play()
     while running:
-        draw_background()
         if stop:
-            pygame.mixer.music.load('Data/level1.mp3')
-            cube_sprite = pygame.sprite.Group()
-            map_sprite, spike_sprite = pygame.sprite.Group(), pygame.sprite.Group()
+            pygame.mixer.music.load(f'Data/{name[:6]}.mp3')
+            map_sprite, spike_sprite, cube_sprite = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
             wave_sprite, portal_sprite = pygame.sprite.Group(), pygame.sprite.Group()
             cube = Cube(cube_sprite)
-            cube.image = pygame.transform.scale(load_image('player_icon1.png'), (100, 100))
-            stop = False
-            isWave = False
-            countJump = 40
-            make_jump = False
-            wave = 0
-            x = 0
+            cube.image, stop, isWave, countJump, make_jump, wave, x = im, False, False, 40, False, 0, 0
             generate_level(name)
             pygame.mixer.music.play()
+        draw_background(name)
         if -x == len_level:
             end_screen()
         else:
@@ -462,11 +505,16 @@ if __name__ == '__main__':
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    make_jump = True
+                    if cube.vy == 0:
+                        make_jump = True
                     if isWave:
                         if wave.v == 5:
                             wave.v = -5
                         else:
+                            wave.v = 5
+                        if wave.rect.y > 1000:
+                            wave.v = -5
+                        if wave.rect.y == 0:
                             wave.v = 5
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
@@ -480,7 +528,6 @@ if __name__ == '__main__':
                 cube_sprite.draw(screen)
                 if make_jump:
                     cube.jump()
-        # check_cube.draw(screen)
         map_sprite.draw(screen)
         spike_sprite.draw(screen)
         portal_sprite.draw(screen)
